187c56a7164d8265ccd2276f8525696d86b30ba3
Brian Alan Suarez <suarezbrianalan@gmail.com>
2024-07-10 03:54:32 -0300

    fix

--- src/app/componentes/panel-administrador/panel-administrador.component.ts
+++ src/app/componentes/panel-administrador/panel-administrador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButton, MatButtonModule} from '@angular/material/button';
@@ -18,6 +18,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { RegistroPacienteComponent } from '../registro/registro-paciente/registro-paciente.component';
 import { RegistroEspecialistaComponent } from '../registro/registro-especialista/registro-especialista.component';
 import { RegistroAdministradorComponent } from '../registro-administrador/registro-administrador.component';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
+
 
 @Component({
   selector: 'app-panel-administrador',
@@ -37,6 +40,8 @@ import { RegistroAdministradorComponent } from '../registro-administrador/regist
   styleUrl: './panel-administrador.component.css'
 })
 export class PanelAdministradorComponent {
+  @ViewChild('content') content!: ElementRef;
+
   administradoresDisplayedColumns: string[] = ['nombre', 'apellido', 'dni', 'edad', 'email', 'fecha_registro', 'password', 'rol', 'rutaArchivoUno'];
   especialistasDisplayedColumns: string[] = ['nombre', 'apellido', 'dni', 'edad', 'email', 'especialidad', 'fecha_registro', 'password', 'rol', 'rutaArchivoUno', 'validarEstado'];
   pacientesDisplayedColumns: string[] = ['nombre', 'apellido', 'dni', 'edad', 'email', 'fecha_registro', 'password', 'rol', 'rutaArchivoUno', 'rutaArchivoDos', 'obraSocial'];
@@ -58,16 +63,84 @@ export class PanelAdministradorComponent {
 
   }
 
+
+  generarExcel(): void {
+
+    const administradoresData = this.administradores.map(usuario => ({
+      Nombre: usuario.nombre,
+      Apellido: usuario.apellido,
+      DNI: usuario.dni,
+      Edad: usuario.edad,
+      Email: usuario.email,
+      Fecha_Registro: usuario.fecha_registro ? new Date(usuario.fecha_registro).toLocaleDateString() : '',
+      Password: usuario.password,
+      Rol: this.obtenerRol(usuario.rol),
+      Ruta_Archivo_Uno: usuario.rutaArchivoUno || '',
+    }));
+  
+    const especialistasData = this.especialistas.map(usuario => ({
+      Nombre: usuario.nombre,
+      Apellido: usuario.apellido,
+      DNI: usuario.dni,
+      Edad: usuario.edad,
+      Email: usuario.email,
+      Especialidad: usuario.especialidades || '(N/A)',
+      Fecha_Registro: usuario.fecha_registro ? new Date(usuario.fecha_registro).toLocaleDateString() : '',
+      Password: usuario.password,
+      Rol: this.obtenerRol(usuario.rol),
+      Ruta_Archivo_Uno: usuario.rutaArchivoUno || '',
+      Validar_Estado: usuario.validarEstado ? 'Si' : 'No',
+    }));
+  
+    const pacientesData = this.pacientes.map(usuario => ({
+      Nombre: usuario.nombre,
+      Apellido: usuario.apellido,
+      DNI: usuario.dni,
+      Edad: usuario.edad,
+      Email: usuario.email,
+      Fecha_Registro: usuario.fecha_registro ? new Date(usuario.fecha_registro).toLocaleDateString() : '',
+      Password: usuario.password,
+      Rol: this.obtenerRol(usuario.rol),
+      Ruta_Archivo_Uno: usuario.rutaArchivoUno || '',
+      Ruta_Archivo_Dos: usuario.rutaArchivoDos || '(N/A)',
+      Obra_Social: usuario.obraSocial || '(N/A)',
+    }));
+  
+    const administradoresSheet = XLSX.utils.json_to_sheet(administradoresData);
+    const especialistasSheet = XLSX.utils.json_to_sheet(especialistasData);
+    const pacientesSheet = XLSX.utils.json_to_sheet(pacientesData);
+  
+    const workbook = {
+      Sheets: {
+        'Administradores': administradoresSheet,
+        'Especialistas': especialistasSheet,
+        'Pacientes': pacientesSheet
+      },
+      SheetNames: ['Administradores', 'Especialistas', 'Pacientes']
+    };  
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });  
+    this.guardarExcel(excelBuffer, 'usuarios');
+  }
+
+  guardarExcel(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8' });
+    saveAs(data, fileName + '_' + new Date().getTime() + '.xlsx');
+  }
+
+  obtenerRol(rol: number): string {
+    switch (rol) {
+      case 1: return 'Administrador';
+      case 2: return 'Especialista';
+      case 3: return 'Paciente';
+      default: return '(N/A)';
+    }
+  }
   async ngOnInit(){
     this.datosUsuario.obtenerCollection('usuarios').subscribe({
       next: (data: any[]) => {   
         const filterAdministrador = data.filter(element => element.rol === 1);
         const filterEspecialista = data.filter(element => element.rol === 2);
         const filterPaciente = data.filter(element => element.rol === 3);
-        this.dataSource = data.map((element) => ({
-          ...element,
-          fecha_registro: (element.fecha_registro as Timestamp).toDate()
-        }));
         this.administradores = filterAdministrador.map((element) => ({
           ...element,
           fecha_registro: (element.fecha_registro as Timestamp).toDate()
